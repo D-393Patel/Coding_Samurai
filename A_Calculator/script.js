6792c5678a80dca413554ff19e49c890928093a3
D-393Patel <143238468+D-393Patel@users.noreply.github.com>
2024-01-05 22:46:34 +0530

    first

---
+++ A_Calculator/script.js
@@ -0,0 +1,52 @@
+let screen = document.querySelector('#screen');
+let btn =document.querySelectorAll('.btn');
+
+for(item of btn){
+  item.addEventListener('click',(e)=>{
+    // console.log(item);
+    btntext = e.target.innerText;
+    // console(btntext);
+    if(btntext =='x'){
+      btntext ='*';
+    }
+    
+    screen.value += btntext;
+  });
+}
+function percent(){
+  screen.value /=100;
+}
+function sin(){
+  screen.value =Math.sin(screen.value);
+}
+function cos(){
+  screen.value= Math.cos(screen.value);
+}
+function tan(){
+  screen.value= Math.tan(screen.value);
+}
+function pow(){
+  screen.value= Math.pow(screen.value,2);
+}
+function sqrt(){
+  screen.value= Math.sqrt(screen.value,2);
+}
+function pi(){
+  screen.value=3.14159265359;
+}
+function e(){
+  screen.value= 2.71838182846;
+}
+function fact(){
+  let i,num,f;
+  f=1;
+  num=screen.value;
+  for(i=1;i<=num;i++){
+    f=f*i;
+  }
+  i=i-1;
+  screen.value =f;
+}
+function backspc(){
+  screen.value = screen.value.substr(0,screen.value.length-1);
+}
